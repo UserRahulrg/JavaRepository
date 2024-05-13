0594899b9715923a10c122605c761b84c322401d
Rahul Gautam <117631348+UserRahulrg@users.noreply.github.com>
2024-05-14 00:56:22 +0530

    Add files via upload
    Repo is Now Updated

---
+++ print_1_7_left_right.java
@@ -0,0 +1,11 @@
+class print_1_7_left_right
+{
+	public static void main(String[] args)
+	{
+		System.out.print("Numbers from 1 to 7 are:");
+		for(int i=0; i<= 7; i++)
+		{
+			System.out.print(i+",");
+		}
+	}
+}
